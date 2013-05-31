d33b9e65ff028ac4ea94c02f1d50230d24708aa8
Joong Lee <jylee@kisti.re.kr>
2013-05-31 16:53:56 +0900

    Implemented session analysis

--- code/session_analysis.py
+++ code/session_analysis.py
@@ -3,8 +3,11 @@ import sys
 import numpy as np
 
 lines = []
-remove_reason = [0, 0]
-remove_lines = 0
+cur_duration = {}
+prev_duration = {}
+next_duration = {}
+num_visit = {}
+
 
 class user_session:
     def __init__(self):
@@ -12,25 +15,23 @@ class user_session:
         self.stats = []        # size is equal to uiddict
         self.timelists = []    # total size is equal to lines
         self.lidlists = []     # total size is equal to lines
-        self.declists = []     # total size is equal to lines
         self.sessionlists = []
+        
 
-    def append(self, uid, time, dec, lid):
+    def append(self, uid, time, lid):
         if uid in self.uiddict:
             idx = self.uiddict[uid]
             self.timelists[idx].append(time)
             self.lidlists[idx].append(lid)
-            self.declists[idx].append(dec)
         else :
             # save index of lists
             self.uiddict[uid] = len(self.timelists)
 
             timelist = [time]
             lidlist = [lid]
-            declist = [dec]
             self.timelists.append(timelist)
             self.lidlists.append(lidlist)
-            self.declists.append(declist)
+
 
     def calcStats(self):
         nuser = len(self.uiddict)
@@ -58,72 +59,71 @@ class user_session:
         print('Length of uiddict is ' + str(len(self.uiddict)) + '\n')
         print('Length of stats is ' + str(len(self.stats)) + '\n')
 
-    def genSession(self):
+
+    def genSessionInfo(self):
         for i in range(len(self.uiddict)):
-            self.sessionlist = []
+            lent = len(self.timelists[i])
 
-            for j in range(len(self.timelists[i])-1):
-                delt = self.timelists[i][j+1]-self.timelists[i][j]
+            session = []
+            prev_session = []
+            prev_delt = 0
 
+            # this list is for visit analysis
+            sessionlist = []
+
+            for j in range(lent-1):
+                
+                delt = self.timelists[i][j+1] - self.timelists[i][j]
+                session.append(j)
+
+                if delt > 0 :
+                    for k in session: 
+                        cur_duration[self.lidlists[i][k]] = delt
+                        prev_duration[self.lidlists[i][k]] = prev_delt
+
+                    for k in prev_session:
+                        next_duration[self.lidlists[i][k]] = delt
+
+                    prev_session = session
+                    session = []
+                    prev_delt = delt
+
+                # for visit analysis
                 if delt > 3600 or (delt > 60 and delt > self.stats[i][2]):
-                    self.sessionlist.append(j)
-#                    print('seperating delta is ' + str(delt))
-#                    print('mean is ' + str(self.stats[i][2]))
+                    sessionlist.append(j)
 
-            self.sessionlist.append(len(self.timelists[i])-1)
-            self.sessionlists.append(self.sessionlist)
+            # append the last session
+            session.append(lent-1)
+            for k in session: 
+                cur_duration[self.lidlists[i][k]] = 0
+                prev_duration[self.lidlists[i][k]] = prev_delt
+                next_duration[self.lidlists[i][k]] = 0
+                    
+            for k in prev_session:
+                next_duration[self.lidlists[i][k]] = 0
 
-        print('Number of sessionlist is ' + str(len(self.sessionlists)))
+            # for visit analysis
+            sessionlist.append(len(self.timelists[i])-1)
+            self.sessionlists.append(sessionlist)
 
-
-    def getDecisionCount(self, uidx, sid, eid):
-        pcnt = ncnt = 0  
-
-        for i in range(eid-sid+1):
-            if self.declists[uidx][i+sid] == 1:
-                pcnt += 1
-            else:
-                ncnt += 1
-
-        return [pcnt, ncnt]
+            if i % 1000 == 0:
+                print('Processed ' + str(i) + ' users')
 
 
-    def removeLines(self, uidx, sid, eid):
-        global remove_lines
-        for i in range(eid-sid+1):
-            lines[self.lidlists[uidx][i+sid]] = '-1\n'
-            remove_lines += 1
-
-
-    def removeSession(self):
+    def visitAnalysis(self):
         for i in range(len(self.uiddict)):
             sid = 0
-            for j in range(len(self.sessionlists[i])):
-                session = self.sessionlists[i][j]
-                count = self.getDecisionCount(i, sid, session)
+            for visit in self.sessionlists[i]:
+                length = visit-sid+1
+                for j in range(length):
+                    num_visit[self.lidlists[i][j+sid]] = length
+            sid = visit+1
 
-                remove = 0
-                size = count[0]+count[1]
 
-                if count[0] == 0:
-                    remove_reason[0] += 1                                 
-                    remove = 1
-                elif count[0] / size > 0.9 and size > 10:
-                    remove_reason[1] += 1                                 
-                    remove = 1
-
-                if remove == 1: 
-                    # remove session
-                    self.removeLines(i, sid, session)
-                    self.sessionlists[i][j] = -1         
-
-                sid = session + 1
-
-    def analysis(self, souttxt):
+    def analysis(self):
         self.calcStats()
-        self.genSession()
-
-        self.removeSession()
+        self.genSessionInfo()
+        self.visitAnalysis()
 
 
 if len(sys.argv) < 2 :
@@ -150,7 +150,7 @@ count = 0
 for line in lines:
     tmp = line.split()
     if len(tmp) > 3 :
-        us.append(int(tmp[0]), int(tmp[3]), int(tmp[2]), count)
+        us.append(int(tmp[0]), int(tmp[3]), count)
     else :
         print(tmp)
 
@@ -160,13 +160,17 @@ for line in lines:
 
 souttxt = open(sys.argv[1] + '.sa', 'w')
 
-us.analysis(souttxt)
+us.analysis()
 
+count = 0
 for line in lines:
-    if line != '-1\n':
-        souttxt.write(line)
+    tmp = line.split()
+    if len(tmp) > 3:
+        souttxt.write(tmp[0] + '\t' + tmp[1] + '\t' +  tmp[2] + '\t' +  str(cur_duration[count]) + '\t'  +  str(prev_duration[count]) + '\t' +  str(next_duration[count]) + '\t' + str(num_visit[count]) + '\n')
+
+    count += 1
+    if count % 1000 == 0 :
+        print('Writing ' + str(count) + ' lines')
 
 souttxt.close()
 
-print('remove reason is ' + str(remove_reason[0]) + ', ' + str(remove_reason[1]))
-print('number of total removed lines is ' + str(remove_lines))
